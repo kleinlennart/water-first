e8656545e9f32694a92fcdb72d6c798cba095129
kleinlennart <31488915+kleinlennart@users.noreply.github.com>
2025-04-19 14:26:28 -0400

    first pass

---
+++ R/01-read_data.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+library(googlesheets4)
+
+URL <- ""
+
+sheet_properties <- googlesheets4::sheet_properties(URL)
+raw <- googlesheets4::read_sheet(URL, sheet = "")
+
+
+
+# -------------------------------------------------------------------------
+
+
+### join the data
+
+
+
+### 
+
+
